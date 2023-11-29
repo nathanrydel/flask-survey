f3ec77c49f62f79b52e308fe1081a5cfaa7eed63
Teagan McClintock <teagan.mcclintock@gmail.com>
2023-11-28 23:17:13 -0500

    Completed ability to choose survey and pass selection through entire form

--- app.py
+++ app.py
@@ -23,10 +23,14 @@ def show_survey_home():
     """Show survey title, instructions, and a button. Clears
     session["responses"] on page load"""
 
+    session[SESSION_RESPONSES_KEY] = []
+
     survey_title = request.form["survey_selection"]
     survey = surveys[survey_title]
 
-    return render_template("survey_start.html", survey=survey)
+    return render_template("survey_start.html",
+                           survey=survey,
+                           survey_key=survey_title)
 
 # TODO: Add survey variables to all non-root endpoints for tracking
 
@@ -35,29 +39,31 @@ def show_survey_home():
 def start_survey():
     """Start the survey"""
 
-    # putting in the show function to clear everytime home page renders
-    session[SESSION_RESPONSES_KEY] = []
+    survey_key = request.form["survey_key"]
 
-    return redirect("/questions/0")
+    return redirect(f"/{survey_key}/questions/0")
 
 
-@app.get("/questions/<int:question_id>")
-def show_question(question_id):
+@app.get("/<survey_key>/questions/<int:question_id>")
+def show_question(survey_key, question_id):
     """Show current question as form; redirects user to next unanswered question
     if they try to access questions out of order, or thank-you page if they've
     already completed the form, with accompanying flash messages"""
 
     responses = session[SESSION_RESPONSES_KEY]
+    survey = surveys[survey_key]
 
     if len(responses) == len(survey.questions):
         flash("You've already answered all the questions!")
-        return redirect("/thanks")
+        return redirect(f"/{survey_key}/thanks")
     elif question_id == len(responses):
         current_question = survey.questions[question_id]
-        return render_template("question.html", question=current_question)
+        return render_template("question.html",
+                               question=current_question,
+                               survey_key=survey_key)
     else:
         flash("Please answer questions in order")
-        return redirect(f"/questions/{len(responses)}")
+        return redirect(f"/{survey_key}/questions/{len(responses)}")
 
 
 @app.post("/answer")
@@ -76,16 +82,20 @@ def handle_question_submission():
     responses.append(answer)
     session[SESSION_RESPONSES_KEY] = responses
 
+    survey_key = request.form["survey_key"]
+    survey = surveys[survey_key]
+
     if len(responses) == len(survey.questions):
-        return redirect("/thanks")
+        return redirect(f"/{survey_key}/thanks")
     else:
-        return redirect(f"/questions/{len(responses)}")
+        return redirect(f"/{survey_key}/questions/{len(responses)}")
 
 
-@app.get("/thanks")
-def thank_user():
+@app.get("/<survey_key>/thanks")
+def thank_user(survey_key):
     """Displays thank you page with survey responses"""
 
+    survey = surveys[survey_key]
     responses = session[SESSION_RESPONSES_KEY]
 
     return render_template("completion.html",
