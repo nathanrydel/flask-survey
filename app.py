46cce41bc7198b3e8028adf10c2236249651004b
Nathan Rydel <nathanrydel@gmail.com>
2023-11-30 13:10:43 -0700

    created root route to render survey_start

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.get("/")
+def show_survey_home():
+    """Show survey title, instructions, and a button"""
+
+    return render_template("survey_start.html", survey=survey)
