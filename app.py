99532b48100ac751072e0c8e510f02cc4173726b
Nathan Rydel <nathanrydel@gmail.com>
2023-11-30 13:12:45 -0700

    add redirect to first question

--- app.py
+++ app.py
@@ -16,3 +16,10 @@ def show_survey_home():
     """Show survey title, instructions, and a button"""
 
     return render_template("survey_start.html", survey=survey)
+
+
+@app.post("/begin")
+def start_survey():
+    """Start the survey"""
+
+    return redirect("/questions/0")
